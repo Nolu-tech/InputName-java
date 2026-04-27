3d45828085397b46a25d7af5ed268c68e78756cd
Nolu-tech <noluthandomasekop@gmail.com>
2026-04-27 20:57:58 +0200

    Add files via upload

---
+++ InputName.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+
+
+public class InputName {
+
+
+
+    public static void main(String[] args) {
+
+
+
+        Scanner input = new Scanner(System.in);
+
+
+
+        System.out.print("Please enter your name: ");
+
+
+
+        String name = input.nextLine();
+
+
+
+        System.out.println("Your name is: " + name);
+
+
+
+        input.close();
+
+
+
+    }
+
+
+
+}
+ 
+    
+      
